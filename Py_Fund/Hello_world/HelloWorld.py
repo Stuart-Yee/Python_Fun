de3ca00bfac36fa5be2f6272b65a5eab0cf9459b
Stuart-Yee <ki6fzb@gmail.com>
2021-07-31 12:07:53 -0700

    more string practice

--- Py_Fund/Hello_world/HelloWorld.py
+++ Py_Fund/Hello_world/HelloWorld.py
@@ -13,4 +13,34 @@ fave_food1 = "sushi"
 fave_food2 = "pizza"
 print( "I love to eat {} and {}.".format(fave_food1, fave_food2) ) # with .format()
 print( f"I love to eat {fave_food1} and {fave_food2}.") # with an f string
+myName = "Stuart"
 
+print("Hello", myName, "!")
+
+print("Hello " + myName + "!")
+
+fav_number = 172.3
+
+print("Hello", fav_number, "!")
+
+#print("Hello " + fav_number + "!")
+
+print("Hello " +str(fav_number) +"!")
+
+fav_food1 = "ribeye steak"
+fav_food2 = "dim sum"
+
+print("I love to eat {} and {}.".format(fav_food1, fav_food2)); #seems to run if you accidentally add a semicolon
+
+print(f"I love to eat {fav_food1} and {fav_food2}.")
+
+print(len(fav_food1))
+
+words = fav_food1.split()
+
+
+print(words)
+
+words += fav_food2.split()
+
+print(words)
